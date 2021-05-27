e686882635f6bb870cca01e06c85b85efb75d78a
liju <liju.g.chacko@gmail.com>
2021-05-27 18:20:30 +0530

    int to size_t

--- example.c
+++ example.c
@@ -18,29 +18,29 @@ int main()
     printf("\n###Identifying Nodes using Query##");
     struct ResultStruct * result= GetNode(DB, 0,  "head*/title[This is document title]");
     //for _, node := range identifiedNodes {
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         printf("\n%s", GetNodeContents(DB, result->nodeids.items[i])->charbuf);
     }
     free_ResultStruct(result);/*Do not forget to free memory if you don't want memory leakage  */
     printf("\n###Getting Value of identified nodes.##");
     result= GetNode(DB, 0, "head/title*");
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         printf("\n%s", GetNodeValue(DB, result->nodeids.items[i])->charbuf);
     }
     printf("\n###Searching using attribute value.##");
     result = GetNode(DB, 0, "body*[style=\"123\"]/h1");
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         printf("\n%s", GetNodeContents(DB, result->nodeids.items[i])->charbuf);
     }
     printf("\n###[2]Searching using attribute value.##");
     result =  GetNode(DB, 0, "<x>*[style=\"123\"]/h1");
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         printf("\n%s", GetNodeContents(DB, result->nodeids.items[i])->charbuf);
     }
 
     printf("\n### Updating node value ##\n");
     result =  GetNode(DB, 0, "head/title");
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         UpdateNodevalue(DB, result->nodeids.items[i], "test_new");
         printf("After updating value\n");
         printf("\n%s", GetNodeContents(DB, result->nodeids.items[i])->charbuf);
@@ -49,27 +49,27 @@ int main()
     struct VectorInt *ids = ChildNodes(DB, result2->nodeids.items[0]);
     printf("\nchildren");
 
-    for(int i=0;i<ids->length;i++){
+    for(size_t i=0;i<ids->length;i++){
         printf("\n%s", GetNodeContents(DB, ids->items[i])->charbuf);
     }
     printf("\nDebug head\n");
     NodeDebug(DB,result2->nodeids.items[0]);
     printf("\n### Updating node ##\n");
-    result = GetNode(DB, 0, "head/title");
-    for(int i=0;i<result->nodeids.length;i++){
-        struct ResultStruct *newnodes = ReplaceNode(DB, result->nodeids.items[i], "<title>test</title>");
+    struct ResultStruct *result3 = GetNode(DB, 0, "head/title");
+    for(size_t i=0;i<result3->nodeids.length;i++){
+        struct ResultStruct *newnodes = ReplaceNode(DB, result3->nodeids.items[i], "<title>test</title>");
         printf("After updation\n");
-        printf("old node value- %s", GetNodeValue(DB, result->nodeids.items[i])->charbuf); //no output, existing id is removed and new id added
+        printf("old node value- %s", GetNodeValue(DB, result3->nodeids.items[i])->charbuf); //no output, existing id is removed and new id added
         printf("\nnew node value- %s", GetNodeValue(DB, newnodes->nodeids.items[0])->charbuf);
     }
     printf("\nAfter updation- research\n");
     result =  GetNode(DB, 0, "head/title");
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         printf("\n%s", GetNodeContents(DB, result->nodeids.items[i])->charbuf);
     }
     printf("\n### Updating node attribute##\n");
     result = GetNode(DB, 0, "<x>*[style=\"123\"]/h1");
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         printf("\n%s", GetNodeAttribute(DB, result->nodeids.items[i], "style")->charbuf);
         UpdateAttributevalue(DB, result->nodeids.items[i], "style", "test2");
         printf("\nafter updating Attribute-\n%s", GetNodeContents(DB, result->nodeids.items[i])->charbuf);
@@ -79,7 +79,7 @@ int main()
     }
     printf("\n### Recursive Search##\n");
     result = GetNode(DB, 0, "../h1");
-    for(int i=0;i<result->nodeids.length;i++){
+    for(size_t i=0;i<result->nodeids.length;i++){
         printf("\n%s", GetNodeContents(DB, result->nodeids.items[i])->charbuf);
     }
 
