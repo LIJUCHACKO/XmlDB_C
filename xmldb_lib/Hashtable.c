01ae8ced76d42e6200d82f19181668924f07ad4f
liju <liju.g.chacko@gmail.com>
2024-02-24 15:58:48 +0530

    bug correction

--- xmldb_lib/Hashtable.c
+++ xmldb_lib/Hashtable.c
@@ -36,52 +36,16 @@ void insertid_intohashtable(struct Hashtable* HashTB,int hashno ,int  nodeId,int
         appendto_VectorInt(Vect,nodeId);
         return;
     }
-    int lineno = reference_linenotoinsert;
-
-    int LowLM = 0;
-    int UpLM = Vect->length - 1;
-    int MidLM = 0;
-    int index = -1;
-    if (nodeNoToLineno->items[HashTB->lists[hashno].items[LowLM]] == nodeNoToLineno->items[HashTB->lists[hashno].items[UpLM]]){
-         if (nodeNoToLineno->items[HashTB->lists[hashno].items[LowLM]] ==lineno){
-             appendto_VectorInt(Vect,nodeId);
-             return;
-         }
-    }
-    while(1) {
-        MidLM = (int)((LowLM + UpLM) / 2);
-        if ((lineno >= nodeNoToLineno->items[Vect->items[LowLM]])  && (lineno < nodeNoToLineno->items[Vect->items[MidLM]])) {
-            if (nodeNoToLineno->items[Vect->items[LowLM]] == nodeNoToLineno->items[Vect->items[MidLM]]) {
-                LowLM = MidLM;
-            } else {
-                UpLM = MidLM;
-            }
-
-        } else if ((lineno >= nodeNoToLineno->items[Vect->items[MidLM]] )&& (lineno <= nodeNoToLineno->items[Vect->items[UpLM]])) {
-            if (nodeNoToLineno->items[Vect->items[UpLM]] == nodeNoToLineno->items[Vect->items[MidLM]]) {
-                UpLM = MidLM;
-            } else {
-                LowLM = MidLM;
-            }
-
-        } else {
-            break;
-        }
-        //fmt.Printf("\n%d  %d  %d", UpLM, MidLM, LowLM)
-        if ((UpLM == LowLM) || (UpLM == (LowLM+1))) {
-            index = UpLM;
-            break;
-        }
+    int index = Vect->length - 1;
+    unsigned long lastno=Vect->length;
+    while(nodeNoToLineno->items[HashTB->lists[hashno].items[index]] > reference_linenotoinsert){
+        lastno=index;
+        index--;
 
     }
-    if (index < 0) {
-        if (lineno < nodeNoToLineno->items[Vect->items[0]]) {
-            index = 0;
-        } else {
-            appendto_VectorInt(Vect,nodeId);
-            return;
-        }
-
+    if(lastno > Vect->length - 1){
+        appendto_VectorInt(Vect,nodeId);
+        return;
     }
     inserto_VectorInt(Vect,index,nodeId);
     return;
